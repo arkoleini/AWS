9224cdd70391bd42dbc33f3853f139d7a2c251b9
Reza Kolini <38354250+arkoleini@users.noreply.github.com>
2023-06-04 17:37:08 -0500

    Update is-auth.js
    fix login page

--- P12-Authentication/middleware/is-auth.js
+++ P12-Authentication/middleware/is-auth.js
@@ -1,7 +1,7 @@
 module.exports =(req, res, next)=>{
     if (!req.session.isLoggedIn){
         console.log(`##### middle ware is-Auth`);
-        return res.redirect('/loggin');
+        return res.redirect('/login');
     }
     next();
 }
